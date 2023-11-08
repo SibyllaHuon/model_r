e1af0417b158e6502033046d0a112831a74d04f5
Huon Research <rohan@huon.ai>
2023-11-08 19:31:31 +0000

    Updated file

--- estimate_models.R
+++ estimate_models.R
@@ -17,39 +17,36 @@ library(rstanarm)
 #### Read data ####
 data <- read_csv("raw_data/responses_with_human_coding.csv")
 
-data <-
-    data |>
-    mutate(Prompt_n = factor(Prompt_n,
-                            levels = c("Name", "Describe", "Simulate", "Example")),
-           Temperature = factor(Temperature),
-           Role_n = factor(Role_n, levels = c("Helpful", "Expert")),
-           Shot_n = factor(Shot_n, levels = c("Zero", "One", "Few")),
-           Version = factor(Version)
-    )
-
-data <- 
-    data |>
-    rowwise() |>
-    mutate(
-        consistency = round(mean(c(consistency_coder_1, consistency_coder_2), na.rm = TRUE)),
-        decency = round(mean(c(decency_coder_1, decency_coder_2), na.rm = TRUE))) |>
-    ungroup()
+# Update mutate functions to use pipe operator
+data <- data %>% 
+  mutate(Prompt_n = factor(Prompt_n,
+                           levels = c("Name", "Describe", "Simulate", "Example")),
+         Temperature = factor(Temperature),
+         Role_n = factor(Role_n, levels = c("Helpful", "Expert")),
+         Shot_n = factor(Shot_n, levels = c("Zero", "One", "Few")),
+         Version = factor(Version)) %>%
+  rowwise() %>%
+  mutate(
+    consistency = round(mean(c(consistency_coder_1, consistency_coder_2), na.rm = TRUE)),
+    decency = round(mean(c(decency_coder_1, decency_coder_2), na.rm = TRUE))) %>%
+  ungroup()
 
 ### Model data ####
 # Use MASS while getting something working
 consistency_model_MASS <-
-    MASS::polr(factor(consistency) ~ Version + Prompt_n + Temperature + Role_n + Shot_n, data = data)
+  MASS::polr(factor(consistency) ~ Version + Prompt_n + Temperature + Role_n + Shot_n, data = data)
 
 decency_model_MASS <-
-    MASS::polr(factor(decency) ~ Version + Prompt_n + Temperature + Role_n + Shot_n, data = data)
+  MASS::polr(factor(decency) ~ Version + Prompt_n + Temperature + Role_n + Shot_n, data = data)
 
 # Use rstanarm for what we report in the paper
+# Add comments explaining the choices of priors and seeds
 consistency_model_rstanarm <-
-    stan_polr(
-        factor(consistency) ~ Version + Prompt_n + Temperature + Role_n + Shot_n,
-        data = data,
-        prior = R2(0.3, "mean"),
-        seed = 853)
+  stan_polr(
+    factor(consistency) ~ Version + Prompt_n + Temperature + Role_n + Shot_n,
+    data = data,
+    prior = R2(0.3, "mean"), # Prior for R-squared, chosen to be 0.3 with a mean prior distribution
+    seed = 853) # Set random seed for reproducibility
 
 saveRDS(
   consistency_model_rstanarm,
@@ -57,11 +54,11 @@ saveRDS(
 )
 
 decency_model_rstanarm <-
-   stan_polr(
-        factor(decency) ~ Version + Prompt_n + Temperature + Role_n + Shot_n,
-        data = data,
-        prior = R2(0.3, "mean"),
-        seed = 853)
+  stan_polr(
+    factor(decency) ~ Version + Prompt_n + Temperature + Role_n + Shot_n,
+    data = data,
+    prior = R2(0.3, "mean"), # Prior for R-squared, chosen to be 0.3 with a mean prior distribution
+    seed = 853) # Set random seed for reproducibility
 
 saveRDS(
   decency_model_rstanarm,
